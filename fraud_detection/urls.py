2dd9bd3d1941669a30856835768e1298eb2a216b
sagar25k <148604865+sagar25k@users.noreply.github.com>
2024-07-27 10:29:49 +0530

    updated

---
+++ fraud_detection/urls.py
@@ -0,0 +1,9 @@
+# FraudDetectionSystem/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('detection.urls')),
+]
